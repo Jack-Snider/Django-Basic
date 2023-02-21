8b8bd5f3b1284d1a30923a2a9456dc48ee4eb79d
Jack Snider <choiman2021@gmail.com>
2023-02-21 20:15:41 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views # 해당 폴더에서 views를 가져와라
 urlpatterns = [
 
     path( '<int:pk>/' , views.single_post_page ), # 정수형 파라미터를 받는데 변수 이름은 pk
-    path( '', views.index ), # 만약 blog 이후에 아무 url도 없다면 views파일에 index함수를 실행
+    path( '', views.PostList.as_view() ), # views 파일에 있는 PostList 클래스를 뷰로 사용하겠다. ( as_view()는 약속이라고 생각하셈 일단 )
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from .models import Post
 
+from django.views.generic import ListView # 장고에서 제공하는 목록을 보여주는 기능
+
 # Create your views here.
 
-def index( request ):
+class PostList( ListView ):
+    model = Post
+    #template_name = 'blog/index.html' # 템플릿을 지정해줌 ( 장고에서 제공 )
+    # 사실  html파일을 post_html과 같이 변경하면 template_name도 필요없음
+    ordering = '-pk'
 
-    '''
-    order_by( 'pk' ) : pk순서대로
-    order_by( '-pk' ) : pk의 역순대로  
-    '''
-    posts = Post.objects.all().order_by( '-pk' )
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# def index( request ):
+#
+#     '''
+#     order_by( 'pk' ) : pk순서대로
+#     order_by( '-pk' ) : pk의 역순대로
+#     '''
+#     posts = Post.objects.all().order_by( '-pk' )
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 def single_post_page( request, pk ):
 
