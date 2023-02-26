a418294f545761b1a5446b0f88bb8f85d0582e25
Jack Snider <choiman2021@gmail.com>
2023-02-26 16:03:35 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post( models.Model ):
         auto_now_add : 새로 만들어졌을 때 현재시간을 자동으로 반영
         auto_now : 수정 되었을 때 자동으로 현재시간을 반영
     '''
+    head_image = models.ImageField( upload_to='blog/images/%Y/%m/%d/', blank=True ) # blog/images/년/월/일 폴더에 저장
     created_at = models.DateTimeField( auto_now_add = True ) # 작성 시간
     updated_at = models.DateTimeField( auto_now = True ) # 수정 시간  
     #author : 추후 작성
--- django_prj/urls.py
+++ django_prj/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path( 'blog/', include( 'blog.urls' ) ), # blog url을 만나면 blog앱 안에 urls.py로 이동하라
     path('admin/', admin.site.urls),
     path( '', include( 'single_pages.urls' ) )
 ]
+
+urlpatterns += static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
