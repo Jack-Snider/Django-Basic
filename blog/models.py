9663408ce38c4a5b74942ba288206421749f2353
= <choiman2021@gmail.com>
2023-04-01 12:09:27 +0900

    주석 추가

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User # 사용자 계정을 위한 장고 
 # like Class == Table name (  also can be Object )
 # fields == Attribute ( Like in Database )
 # Object -> Post .. .. ( Can be other class also )
+# Django에서 models.py에 있는 각각의 클래스는 장고에서 제공하는 DB에 테이블로 들어간다. ( 클래스 이름 : 테이블 이름, 클래스 안에 있는 필드 : 속성 ) → 클래스로 찍어낸 하나의 객체가 DB로 치면 하나의 튜플
 class Post( models.Model ):
     title = models.CharField(max_length=50)  # 게시물의 제목 , 최대 글자 50자
     hook_text = models.CharField(max_length=100, blank=True)  # 블로그 내용 요약을 위한 char필드
@@ -28,7 +29,7 @@ class Post( models.Model ):
     # 사용자
     # User객체가 삭제될 경우 그 객체를 외래키로 참고하고 있는 테이블의 튜플도 함께 삭제
     # on_delete = models.CASCADE → 해당 유저가 삭제됬을 때 그 유저와 관련된 모든 데이터도 삭제( 예 : 게시글 )
-    # on_delete = models.SET_NULL → 해당 유저가 삭제되도 그 유저와 관련된 데이터의 주인이 None으로 바뀐다. ( 즉, 게시글과 같은 데이터의 소유자가 None으로 변경이 됨 )
+    # on_delete = models.SET_NULL → 해당 유저가 삭제되도 그 유저와 관련된 데이터의 주인이 None으로 바뀐다. ( 즉, 게시글과 같은 데이터의 소유자가 None으로 변경이 됨 [ 게시글 작성자에 대하여 삭제된 작성자라면 None으로 표시하게 된다. ] )
     # null = True → null을 허용하는 옵션
     author = models.ForeignKey( User, null=True ,on_delete = models.SET_NULL )
 
